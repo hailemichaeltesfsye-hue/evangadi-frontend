950013abd8ec78ba2ce10e999151826ba85afbae
hailemichael tesfaye <your.hailemichaeltesfsye@gmail.com>
2026-09-16 04:19:07 +0300

    Use environment variable for backend API URL

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosBase = axios.create({
-  baseURL: 'http://localhost:10000/api'
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:10000/api'
 })
 
 axiosBase.interceptors.request.use((config) => {
